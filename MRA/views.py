1c2ac6221baed2faa26e7c53091cb20eea4b99e1
smithc0222 <smithc0222@gmail.com>
2016-09-30 11:49:20 -0400

    List Multiple model objects in view

--- MRA/views.py
+++ MRA/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
-from django.template import loader
+from django.template import loader, RequestContext
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.views.generic import TemplateView
 from django.utils import timezone
 
 from .models import *
 
-class IndexView(generic.ListView):
-    model = Employee
+class IndexView(TemplateView):
     template_name = 'MRA/index.html'
-    context_object_name = 'employee_list'
 
-    def get_queryset(self):
-        return Employee.objects.all()
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['unit_list'] = Unit.objects.all()
+        context['employee_list'] = Employee.objects.all()
+        return context
